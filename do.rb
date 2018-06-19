81bd31bbc92949a44702330e26b84eee56dfc34b
Pablo Marti <pablo@moonfarmer.com>
2018-06-19 16:00:13 -0400

    adds descending order to script; the descending order function takes any non-negative number; add a way to pass script argument; add call to function

--- do.rb
+++ do.rb
@@ -1,1 +1,14 @@
 #!/usr/bin/env ruby
+
+def descending_order(n)
+  if n < 0
+    raise "negative Number"
+  else
+  end
+end
+
+
+# Grab the first argument passed to script
+n = ARGV[0]
+
+descending_order(n.to_i)

b94509857f5d937809c9a455776d2e68982cf345
Pablo Marti <pablo@moonfarmer.com>
2018-06-19 16:07:45 -0400

    make sure to always return a sorted array in reverse; and stdout the value

--- do.rb
+++ do.rb
@@ -4,11 +4,12 @@ def descending_order(n)
   if n < 0
     raise "negative Number"
   else
+    return n.digits.sort.reverse.join.to_i
   end
 end
 
 
 # Grab the first argument passed to script
 n = ARGV[0]
-
-descending_order(n.to_i)
+highest_value = descending_order(n.to_i)
+puts highest_value
